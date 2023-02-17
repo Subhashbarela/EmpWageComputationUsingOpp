88cffa395a11b6e7e3aef015faf1827c9dd76965
Subhash Barela <barelasubhash16@gmail.com>
2023-02-17 04:11:17 -0800

    welcome to Opps paradigm /methodology

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationUsingOpps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Opps Programing Methodology :..");
+        }
+    }
+}
